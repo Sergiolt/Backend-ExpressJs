faa8e1aea522cdbf3d4d9ff081100998e6d06940
Sergio <sergiolopeztorres97@gmail.com>
2022-07-24 20:03:50 +0200

    a

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
